f0e5e3adcdac3a56de9e658520908cc4411e2e72
Pugsy <nimsarakarunaratne@gmail.com>
2025-08-03 00:14:36 +0530

    Completing SignUp page backend. TO DO: implement LogIn page backend

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets{
+  static const supabaseUrl = 'https://vqktvtwsglzthdqdepai.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxa3R2dHdzZ2x6dGhkcWRlcGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNTEyMDUsImV4cCI6MjA2OTcyNzIwNX0.cVATIU8p4CUQpKxbCY6_72hkrS1Ybs9fMbLv5-p3o8U';
+}
---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,16 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:blog_app/core/error/failure.dart';
+
+
+abstract interface class AuthRepository{
+    Future<Either<Failure, String>> signUpWithEmailPassword({
+      required String name,
+      required String email,
+      required String password,
+    });
+
+    Future<Either<Failure, String>> logInWithEmailPassword({
+      required String email,
+      required String password,
+    });
+}
